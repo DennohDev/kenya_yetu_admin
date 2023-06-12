51a4bde24da6f3214b94e5bdd8d277d5bb09978d
DennohDev <dennismaina1024@gmail.com>
2023-06-12 09:38:20 +0300

    fixed the bug where bottom navigation bar was disappearing

--- lib/ui/auth_page.dart
+++ lib/ui/auth_page.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kenya_yetu_admin/ui/splash_screen.dart';
-import 'home_Screen.dart';
+
+import 'bottom_navigation_controller.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot){
          // If User is logged in
           if (snapshot.hasData) {
-            return const HomeScreen();
+            return const BottomNavigation();
           }
 
          // If User is not logged in
